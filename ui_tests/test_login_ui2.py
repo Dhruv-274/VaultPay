87c8554d3a3cb45efee49c8dd53020e12d663bce
Dhruv Sudarshan <134755715+Dhruv-274@users.noreply.github.com>
2025-08-02 14:46:42 +0530

    Update test_login_ui2.py

--- ui_tests/test_login_ui2.py
+++ ui_tests/test_login_ui2.py
@@ -5,7 +5,7 @@ import time
 
 def test_login_ui():
     options = Options()
-    options.add_argument("--headless")  # Run without GUI
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
